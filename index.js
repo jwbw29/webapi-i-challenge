c63088c18a194195d26e17c3c8bfeca8451619cd
Justin Byrd <justinbyrd7@gmail.com>
2023-11-14 12:56:35 -0600

    server up

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
 // implement your API here
- 
+// in nodejs, we don't import -- we use require
+
+const express = require("express"); // importing a CommonJS module
+
+const server = express(); //gives us an instance of a server powered by Express
+
+server.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+server.listen(8000, () => console.log(`API running on ${8000}`));
